f343f6ea0f952b024716bdbac49a304f1fa4764b
Spinchair <johnwilliams94@hotmail.com>
2015-12-15 19:10:04 +1100

    Made a button that will be removed, still learning how to do stuff and how to code in java.

--- app/src/main/java/com/android/spinchair/unitracker/MainActivity.java
+++ app/src/main/java/com/android/spinchair/unitracker/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
